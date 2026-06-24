9719cdd619a954bfe945c376f527f568b978e3cc
akshatbhatt166 <doccodfine@gmail.com>
2026-06-24 19:30:13 +0530

    Fix formatting issues in 04.c

--- 04.c
+++ 04.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d", &num);
 
     if (num == 0) {
-        count = 1; // special case for 0
+        count = 1; 
     } else {
         while (num != 0) {
             num /= 10;
