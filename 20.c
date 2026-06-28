35180c28e059013dbd0fa987a38ee31536dc8f90
akshatbhatt166 <doccodfine@gmail.com>
2026-06-28 23:24:27 +0530

    Add files via upload

---
+++ 20.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, largest = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n % 2 == 0)
+    {
+        largest = 2;
+        n /= 2;
+    }
+
+    for(int i = 3; i * i <= n; i += 2)
+    {
+        while(n % i == 0)
+        {
+            largest = i;
+            n /= i;
+        }
+    }
+
+    if(n > 2)
+        largest = n;
+
+    printf("Largest Prime Factor = %d", largest);
+
+    return 0;
+}
